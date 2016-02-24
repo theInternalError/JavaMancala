3287fae6084f1cfbe99e7726a1ba91e60e1dc9e7
theInternalError <chekov.trekkie@gmail.com>
2016-02-23 22:46:11 -0800

    Mancala driver rev.A
    Added handling for the end conditions. Yes, there is a bit of a memory leak with the Scanner.

--- Mancala.java
+++ Mancala.java
@@ -9,12 +9,19 @@ public class Mancala {
 		MancalaBoard board = new MancalaBoard();
 		
 		// game variables
-		int side = 1, choice = 0, last;
+		int side = 1, choice = 0, last, status = 0;
 		String input;
 		boolean won = false;
 		
 		// main game loop
 		while (!won) {
+			// check for end conditions
+			status = board.checkEnd();
+			if (status != 0) {
+				won = true;
+				continue;
+			}
+			
 			// prompt player for input
 			System.out.println("Player " + side + "'s turn:");
 			board.printBoard();
@@ -23,11 +30,13 @@ public class Mancala {
 		    input = in.nextLine();
 			choice = Integer.parseInt(input);
 			choice--;
-			if (side == 2) choice += 7;
+			if (side == 2)
+				choice += 7;
 			
 			// play the selected move
 			last = board.pick(choice, side);
-			if (last == (side*7 - 1)) continue;
+			if (last == (side*7 - 1))
+				continue;
 			else {
 				board.checkCapture(last, side);
 				
@@ -36,8 +45,10 @@ public class Mancala {
 				side = (side == 1) ? 2 : 1;
 			}
 		}
-		
-		//in.close();
+		if (status > 0)
+			System.out.println("Player " + status + " won!");
+		else
+			System.out.println("Stalemate!");
 		return;
 	}
 
